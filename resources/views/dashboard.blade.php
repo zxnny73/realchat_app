c2b48a63ce8b2364ceedfac26bc1bfab9d430e3a
zxnny73 <mishaklukin48@mail.ru>
2024-02-26 17:43:54 +0400

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <a href="/chatify">Перейти в чат</a>
+</x-app-layout>
